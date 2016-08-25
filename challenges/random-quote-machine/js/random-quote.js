fad38ef483c5f76b58b47f5e58ca0fc552bd1c13
Carlos Bello <cbello@tuenti.com>
2016-08-25 13:11:31 +0200

    Prevent caching ajax quotes requests and remove tags for Tweetter share button

--- challenges/random-quote-machine/js/random-quote.js
+++ challenges/random-quote-machine/js/random-quote.js
@@ -3,16 +3,22 @@
 var TWITTER_SHARE_URL = '//twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=';
 var QUOTE_SERVICE_URL = '//quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
 
+function removeTags(text) {
+  return text.replace(/<\/?\w+>/gi, '');
+}
+
 function updateQuote(response) {
   var quote = response.shift();
   $('#quote-content').html(quote.content);
   $('#quote-author').text(quote.title || 'Unknown');
-  $('#tweet-quote').attr('href', TWITTER_SHARE_URL + encodeURIComponent(quote.content));
+  $('#tweet-quote').attr('href', TWITTER_SHARE_URL + encodeURIComponent(removeTags(quote.content)));
 }
 
 function getQuote() {
   $.getJSON(QUOTE_SERVICE_URL, updateQuote);
 }
+
+$.ajaxSetup({ cache: false });
 
 $(function () {
   $('#new-quote').click(getQuote);
