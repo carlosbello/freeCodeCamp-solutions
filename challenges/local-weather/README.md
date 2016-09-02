0fb625706fa955822e85a5eaa4fe61e30bd4e877
Carlos Bello <cbello@tuenti.com>
2016-09-02 19:39:15 +0200

    Query location and weather services info

--- challenges/local-weather/README.md
+++ challenges/local-weather/README.md
@@ -4,4 +4,5 @@ This a simple solution to the exercise from https://www.freecodecamp.com/challen
 
 This code has been designed as simple as possible to adapt it for hosting at [Codepen](http://codepen.io)
 
-The result can be seen at https://carlosbello.github.io/freeCodeCamp-solutions/challenges/local-weather
+The result can *not* be seen here (at github.io) because some of the api used don't support https access.
+So, if still available, try to see it working at http://codepen.io/carlosbello/full/qaBykP.
--- challenges/local-weather/js/weather.js
+++ challenges/local-weather/js/weather.js
@@ -1,7 +1,65 @@
 /*
 TODO: Implement
-1. Get current location
-2. Query https://openweathermap.org/current#geo
+-1.-Get current location---------------------------
+-2.-Query https://openweathermap.org/current#geo---
 3. Switch between Celsius and Fahrenheit
 4. Show different icons or background depending on the weather
 */
+var units = 'metric';
+
+function requestClientLocation() {
+  var result = $.Deferred();
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        result.resolve(position.coords);
+      },
+      function () {
+        result.reject(':( Sorry but the geolocation failed');
+      }
+    );
+  } else {
+    result.reject("Browser doesn't support Geolocation");
+  }
+  return result.promise();
+}
+
+function showWeatherForLocation(coords) {
+  $.getJSON('http://api.openweathermap.org/data/2.5/weather',
+    {
+      lat: coords.latitude,
+      lon: coords.longitude,
+      units: units,
+      APPID: 'bca17282d99ff745377ed04bc860a0b0'
+    },
+    function (result) {
+      $('#place').text($('#place').text() || (result.name + ', ' + result.sys.country));
+      $('#temperture').text(result.main.temp);
+      $('#description').text(result.weather[0].main);
+      $('#image').attr('src', 'http://openweathermap.org/img/w/' + result.weather[0].icon + '.png');
+    }
+  );
+  showCityInfo(coords);
+}
+
+function showCityInfo(coords) {
+  $.getJSON('http://api.geonames.org/findNearbyPlaceNameJSON',
+    {
+      lat: coords.latitude,
+      lng: coords.longitude,
+      username: 'carlosbello'
+    },
+    function (result) {
+      if (result.geonames) {
+        var address = result.geonames[0];
+        $('#place').text((address.adminName1 || address.name) + ', ' + address.countryCode);
+      }
+    }
+  );
+}
+
+$(function () {
+  requestClientLocation()
+    .done(showWeatherForLocation)
+    .fail(alert);
+});
